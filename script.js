96a7e7dab8b622eb61fc6536132e11bfefed1e3a
AnnieTheAvatar <annielacoba@yahoo.com>
2023-06-02 23:41:19 +1000

    updated the graphs and content

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // create the unpack function from tutorials
 const unpack = (data, key) => data.map(row => row[key]);
 
+const colorway = ['#3fc1c0','#00b2ca','#0899ba','#0f80aa','#1c558e'];
+
 //import and read the data
 Plotly.d3.csv("data/environmental-footprint-milks.csv", footprintData => {
     //define each of the data points to be plotted on the chart
@@ -18,7 +20,10 @@ Plotly.d3.csv("data/environmental-footprint-milks.csv", footprintData => {
             type: 'sort',
             target: 'y',
             order: 'descending'
-        }]
+        }],
+        marker: {
+            color: colorway
+        }
     }];
 
     const water_layout = {
@@ -30,8 +35,14 @@ Plotly.d3.csv("data/environmental-footprint-milks.csv", footprintData => {
             title: 'Water Usage (L)',
         },
         hovermode: 'closest',
-        width: '500px'
-
+        width: 600,
+        font: {
+            family: 'Arial, sans script',
+            size: 15,
+            color: '#28587B'
+        },
+        paper_bgcolor: 'rgba(0, 0, 0, 0)',
+        plot_bgcolor: 'rgba(0, 0, 0, 0)',
     }
 
     //plot the bar chart with the pop
@@ -49,7 +60,10 @@ Plotly.d3.csv("data/environmental-footprint-milks.csv", footprintData => {
             type: 'sort',
             target: 'y',
             order: 'descending'
-        }]
+        }],
+        marker: {
+            color: colorway
+        }
     }];
 
     const land_layout = {
@@ -61,25 +75,71 @@ Plotly.d3.csv("data/environmental-footprint-milks.csv", footprintData => {
             title: 'Land Usage (m2)',
         },
         hovermode: 'closest',
-        width: '500px'
-    }
+        width: 600,
+        font: {
+            family: 'Arial, sans script',
+            size: 15,
+            color: '#28587B'
+        },
+        paper_bgcolor: 'rgba(0, 0, 0, 0)',
+        plot_bgcolor: 'rgba(0, 0, 0, 0)'
+    };
 
     //plot the bar chart with the pop
     Plotly.newPlot('landBar', land_data, land_layout);
+
+    const co2_y = unpack(footprintData, "GHG emissions of milks (kg CO2eq)");
+
+    const co2data = [{
+        x: milk_name,
+        y: co2_y,
+        type: 'bar',
+        transforms: [{
+            type: 'sort',
+            target: 'y',
+            order: 'descending'
+        }],
+        marker: {
+            color: colorway
+        }
+    }];
+
+    const co2layout = {
+        title: 'CO2 Production per Litre of Milk',
+        xaxis: {
+            title: 'Milk Type'
+        },
+        yaxis: {
+            title: 'CO2 Production (kg)',
+        },
+        hovermode: 'closest',
+        width: 600,
+        font: {
+            family: 'Arial, sans script',
+            size: 15,
+            color: '#28587B'
+        },
+        paper_bgcolor: 'rgba(0, 0, 0, 0)',
+        plot_bgcolor: 'rgba(0, 0, 0, 0)'
+    };
+
+    Plotly.newPlot('co2graph', co2data, co2layout);
 
 });
 
 const first_milky = document.getElementById('milky_first');
 const milky_button1 = document.getElementById('milky_first_button');
 const hide_button1 = document.getElementById('hide_btn_1');
-const main = document.getElementById("body");
+const main = document.getElementById("all");
 
 function showMilky1() {
     first_milky.classList.remove("hidden");
+    //main.classList.add("blackout");//
 }
 
 function hideMilky1() {
     first_milky.classList.add("hidden");
+    //main.classList.remove("blackout");//
 }
 
 console.log(hide_button1)
@@ -94,11 +154,12 @@ const hide_button2 = document.getElementById('hide_btn_2');
 
 function showMilky2() {
     second_milky.classList.remove("hidden");
-
+    //main.classList.add("blackout");//
 }
 
 function hideMilky2() {
     second_milky.classList.add("hidden");
+    //main.classList.remove("blackout");//
 }
 
 console.log(hide_button2)
